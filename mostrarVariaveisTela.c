db938f8913fdb63dd68d3a4bf81b7d305ce27ea5
Rafael Luiz de Oliveira <159972106+lzfael@users.noreply.github.com>
2025-04-09 18:24:28 +0000

    Declarando e mostrando variaveis na tela

---
+++ mostrarVariaveisTela.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char nome[20] = "Rafael"; 
+    int idade = 18;
+
+    printf("Olá, meu nome é %s \n", nome);
+    printf("Tenho %d anos \n", idade);
+
+    return 0;
+}
