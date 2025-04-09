78535d6e1a93078defb936c581226bdbc26ff284
Rafael Luiz de Oliveira <159972106+lzfael@users.noreply.github.com>
2025-04-09 18:11:46 +0000

    Aprendendo declarações de variaveis

---
+++ .vscode/declarar.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+int main() {
+    int idade;
+    int quantidade = 1;
+    float altura = 1.80;
+    double peso = 74.5;
+    char letra = "S";
+    char nome[20] = "Rafael";
+
+    quantidade = 10;
+
+    return 0;
+}
